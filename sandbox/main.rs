1164d33672d18e90b9a6d11ced56fed28a190b2d
Mathijs Follon <mathijs.follon@gmail.com>
2026-06-05 17:10:01 +0200

    Update sandbox

--- sandbox/main.rs
+++ sandbox/main.rs
@@ -1,3 +1,8 @@
+/// This is where you can do integration testing,
+/// explore existing features,
+/// or just build a game.
+/// This folder and it contents will be ignored so anything you do here is just local.
+
 pub fn main() {
     println!("Hello, world!");
 }
